6f84a8f0bcad3fe0976c5c9c0da9c38b9b2d3506
fitria-iput <114454458+fitria-iput@users.noreply.github.com>
2022-09-27 12:46:42 +0700

    Add files via upload

---
+++ function.php
@@ -0,0 +1,42 @@
+<?php
+$data=[
+    [
+        "identitas" => "fitria<br>
+        01-12-2005<br>
+        cimerak<br>",
+    "foto" => "fit.webp"
+    ],
+    [
+        "identitas" => "nadira<br>
+        28-01-2005<br>
+        lewianyar<br>",
+    "foto" => "nad.jpg"
+    ],
+    [
+        "identitas" => "ratma<br>
+        24-02-2005<br>
+         lewianyar<br>",
+    "foto" => "rat.jpg"
+    ]
+    ];
+    $produk=[
+    [
+        "nama_p" => "Rosgul",
+     "harga" => "5K",
+    "stock" => "25",
+    "foto" => "rosgul.jpg"
+    ],
+    [
+        "nama_p" => "Bronis",
+     "harga" => "10K",
+    "stock" => "20",
+    "foto" => "bronid.JPG"
+    ],
+    [
+        "nama_p" => "Pisang crispy",
+     "harga" => "2,5K",
+    "stock" => "15",
+    "foto" => "pisang.JPG"
+    ]
+];
+?>
---
+++ produk.php
@@ -0,0 +1,71 @@
+<?php
+include("function.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <link href="style.css" rel="stylesheet" type="text/css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Noto+Sans+Mono:wght@300&display=swap" rel="stylesheet">
+    <title>Produk PKK</title>
+</head>
+<body>
+    <nav>
+    <div>
+        <h3>Rosgul</h3>
+        </div>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="produk.php">Produk</a></li>
+            <li><a href="">Contact</a></li>
+        </ul>
+        </nav>
+    </table>
+    <hr>
+    </table>
+    <center>
+    <table border="1">
+        <th> <img src="rosgul.jpg" width="200"> </th>
+              <tr>
+                <td>
+                Nama : ROSGUL <br>
+                Harga : 5k <br>
+                Deskripsi : Renyah dan gurih,dijamin ketagihan <br>
+               <center>
+               <a href="https://instagram.com/rakgh_food?igshid=YmMyMTA2M2Y="><button>Pesan</button></a>
+               </center>
+                  </td>
+                </tr>
+                <br>
+    <table border="1">
+        <th> <img src="bronis.JPG" width="200"> </th>
+              <tr>
+                <td>
+                Nama : Bronis <br>
+                Harga : 20k <br>
+                Deskripsi : Makanan manis  <br>
+                <center>
+               <a href="https://instagram.com/rakgh_food?igshid=YmMyMTA2M2Y="><button>Pesan</button></a>
+               </center>
+                  </td>
+                </tr>
+                  </td>
+                </tr>
+                <br>
+   <table border="1">
+        <th> <img src="pisang.JPG" width="200"> </th>
+              <tr>
+                <td>
+                Nama : Pisang crispy <br>
+                Harga : 3K <br>
+                Deskripsi : Makanan manis bisa bikin ketagihan degan crispynya <br>
+                <center>
+                <a href="https://instagram.com/rakgh_food?igshid=YmMyMTA2M2Y="><button>Pesan</button></a>
+                </center>
+                  </td>
+                </tr>
+ </html>
